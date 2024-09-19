59f9c65ca3c5fb5422c60489edd2ffbca2a90b7b
takeda-y1026 <takeda-y@roboken.iit.tsukuba.ac.jp>
2024-09-20 00:38:07 +0900

    change

--- joy_launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-# sudo chmod 777 /dev/ttyUSB0
-ros2 launch whill_bringup whill_launch.py
-ros2 run joy joy_node --ros-args -r /joy:=/whill/controller/joy
--- whill_launch.sh
+++ whill_launch.sh
@@ -9,3 +9,5 @@ source ~/.bashrc
 echo whill_bringup whill_launch.pyを起動します
 # sudo chmod 777 /dev/ttyUSB0
 ros2 launch whill_bringup whill_launch.py
+
+bash 
